c3fe0c9ac3073ed54e3b861bad23d00eeb2a4548
ant <anthony@xebix.com>
2015-12-03 16:18:37 -0600

    changed db_connection in functions.php to use constants DB_HOST, DB_USER, DB_PASS, DB_NAME instead of hard coded information

--- classes/functions.php
+++ classes/functions.php
@@ -17,7 +17,7 @@ class grab_cover {
 		$file = fopen ( $url, "rb" );
 		if ($file) {
 			$newf = fopen ( $newfname, "wb" );
-			
+
 			if ($newf) {
 				while ( ! feof ( $file ) ) {
 					fwrite ( $newf, fread ( $file, 1024 * 8 ), 1024 * 8 );
@@ -26,11 +26,11 @@ class grab_cover {
 				die ( 'Could not write cover image file.' );
 			}
 		}
-		
+
 		if ($file) {
 			fclose ( $file );
 		}
-		
+
 		if ($newf) {
 			fclose ( $newf );
 		}
@@ -47,7 +47,7 @@ class grab_cover {
  * seriesList
  * </pre>
  * @author asanchez
- *        
+ *
  */
 class comicSearch {
 	private $db_connection;
@@ -70,23 +70,23 @@ class comicSearch {
 	public $original_purchase;
 	/**
 	 * List of comic series in database
-	 * 
+	 *
 	 * @var ArrayObject
 	 */
 	public $series_list_result;
-	
+
 	/**
 	 * Looks up a comic from the ComicRack
-	 * 
-	 * @param string $series_name        	
-	 * @param int $issue_no        	
+	 *
+	 * @param string $series_name
+	 * @param int $issue_no
 	 */
 	public function comicrackLookup($series_name, $issue_no) {
 		$this->db_connection = new mysqli ( 'chronos', 'comicrack', 'comicrack', 'comicrack' );
 		if ($this->db_connection->connect_errno) {
 			die ( "Connection failed: " );
 		}
-		
+
 		$sql = "SELECT id, data FROM comics WHERE data LIKE '%<Series>%$series_name%<Number>$issue_no</Number>%'";
 		$result = $this->db_connection->query ( $sql );
 		if ($result->num_rows >= 1) {
@@ -96,15 +96,15 @@ class comicSearch {
 	}
 	/**
 	 * Looks up a single comic issue using comic_id
-	 * 
-	 * @param int $comic_id        	
+	 *
+	 * @param int $comic_id
 	 */
 	public function issueLookup($comic_id) {
-		$this->db_connection = new mysqli ( 'chronos', 'comicdb', 'comicdb', 'comicdb' );
+		$this->db_connection = new mysqli ( DB_HOST, DB_USER, DB_PASS, DB_NAME );
 		if ($this->db_connection->connect_errno) {
 			die ( "Connection failed: " );
 		}
-		
+
 		$sql = "SELECT comics.wiki_id, comics.series_id, comics.comic_id, series.series_name, comics.issue_number, comics.release_date, comics.story_name, comics.cover_image, comics.plot, comics.original_purchase FROM comics LEFT JOIN series ON comics.series_id=series.series_id WHERE comics.comic_id = $comic_id";
 		$result = $this->db_connection->query ( $sql );
 		if ($result->num_rows > 0) {
@@ -134,15 +134,15 @@ class comicSearch {
 	}
 	/**
 	 * Looks up the artist of a given comic using comic_id
-	 * 
-	 * @param int $comic_id        	
+	 *
+	 * @param int $comic_id
 	 */
 	public function artistLookup($comic_id) {
-		$this->db_connection = new mysqli ( 'chronos', 'comicdb', 'comicdb', 'comicdb' );
+		$this->db_connection = new mysqli ( DB_HOST, DB_USER, DB_PASS, DB_NAME );
 		if ($this->db_connection->connect_errno) {
 			die ( "Connection failed: " );
 		}
-		
+
 		$sql = "SELECT artist_name FROM artist_link LEFT JOIN artists ON (artists.artist_id = artist_link.artist_id) WHERE artist_link.comic_id = $comic_id";
 		$result = $this->db_connection->query ( $sql );
 		if ($result->num_rows > 0) {
@@ -150,22 +150,22 @@ class comicSearch {
 				$this->artist = $row ['artist_name'];
 			}
 		}
-		
+
 		if ($result->num_rows == 0) {
 			$this->artist = "";
 		}
 	}
 	/**
 	 * Looks up the writer of a given comic using comic_id
-	 * 
-	 * @param int $comic_id        	
+	 *
+	 * @param int $comic_id
 	 */
 	public function writerLookup($comic_id) {
-		$this->db_connection = new mysqli ( 'chronos', 'comicdb', 'comicdb', 'comicdb' );
+		$this->db_connection = new mysqli ( DB_HOST, DB_USER, DB_PASS, DB_NAME );
 		if ($this->db_connection->connect_errno) {
 			die ( "Connection failed: " );
 		}
-		
+
 		$sql = "SELECT writer_name FROM writer_link LEFT JOIN writers ON (writers.writer_id = writer_link.writer_id) WHERE writer_link.comic_id = $comic_id";
 		$result = $this->db_connection->query ( $sql );
 		if ($result->num_rows > 0) {
@@ -173,18 +173,18 @@ class comicSearch {
 				$this->writer = $row ['writer_name'];
 			}
 		}
-		
+
 		if ($result->num_rows == 0) {
 			$this->writer = "";
 		}
 	}
 	/**
 	 * Lists issues of a given series
-	 * 
-	 * @param int $series_id        	
+	 *
+	 * @param int $series_id
 	 */
 	public function issuesList($series_id) {
-		$this->db_connection = new mysqli ( 'chronos', 'comicdb', 'comicdb', 'comicdb' );
+		$this->db_connection = new mysqli ( DB_HOST, DB_USER, DB_PASS, DB_NAME );
 		if ($this->db_connection->connect_errno) {
 			die ( "Connect failed:" );
 		}
@@ -213,11 +213,11 @@ class comicSearch {
 	 * Returns a list of comic series
 	 */
 	public function seriesList() {
-		$this->db_connection = new mysqli ( 'chronos', 'comicdb', 'comicdb', 'comicdb' );
+		$this->db_connection = new mysqli ( DB_HOST, DB_USER, DB_PASS, DB_NAME );
 		if ($this->db_connection->connect_errno) {
 			die ( "Connection failed:" );
 		}
-		
+
 		$sql = "SELECT * FROM series";
 		$this->series_list_result = $this->db_connection->query ( $sql );
 	}
