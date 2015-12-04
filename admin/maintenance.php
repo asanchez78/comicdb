48a7a251abce4439759166c96e765805d6408588
asanchez <asanchez@asanchez-deskto.internal.sitecontrols.com>
2015-12-04 16:21:30 -0600

    add code to get wiki_id for any records that do not have it

--- admin/maintenance.php
+++ admin/maintenance.php
@@ -15,23 +15,33 @@ $returnedResults = new wikiQuery();
 
 if ($login->isUserLoggedIn () == true) {
 	//Get wiki ids for records that do not have them by searching the marvel wikia api
-	$sql = "SELECT comics.comic_id, series.series_name, comics.issue_number
-			FROM comics
-			LEFT JOIN series ON comics.series_id=series.series_id
-			WHERE comics.wiki_id IS NULL";
-	$result = $connection->query ($sql);
-	if ($result->num_rows > 0) {
-		while ($row = $result->fetch_assoc()) {
-			$comic_id = $row ['comic_id'];
-			$series_name = $row ['series_name'];
-			$issue_number = $row['issue_number'];
-			$query = $series_name . " " . $issue_number;
-			$returnedResults->wikiSearch($query, $series_name, $issue_number, 1);
-		}
-	}
-/**	
+//	$sql = "SELECT comics.comic_id, series.series_name, comics.issue_number
+//			FROM comics
+//			LEFT JOIN series ON comics.series_id=series.series_id
+//			WHERE comics.wiki_id IS NULL";
+//	$result = $connection->query ($sql);
+//	if ($result->num_rows > 0) {
+//		while ($row = $result->fetch_assoc()) {
+//			$comic_id = $row ['comic_id'];
+//			$series_name = $row ['series_name'];
+//			$issue_number = $row['issue_number'];
+//			$query = $series_name . " " . $issue_number;
+//			$returnedResults->wikiSearch($query, $series_name, $issue_number, 1);
+//			$sql = "UPDATE comics
+//			SET wiki_id=$returnedResults->wikiSearchResultID
+//			WHERE comic_id='$comic_id'";
+//			echo $sql . "<br/><br/>";
+//			set_time_limit(0);
+//			if (mysqli_query ( $connection, $sql )) {
+//				echo "Record updated successfully with the following information";
+//			} else {
+//				echo "Error: " . $sql . "<br>" . mysqli_error ( $connection );
+//			}
+//		}
+//	}
+
 	//Update records that have a wiki id, but have not been updated with information from the marvel wikia
-	$sql = "SELECT comics.comic_id, comics.wiki_id 
+	$sql = "SELECT comics.comic_id, comics.wiki_id
 			FROM comics
 			WHERE comics.wiki_id IS NOT NULL
 			AND comics.wikiUpdated=0 LIMIT 10";
@@ -48,18 +58,19 @@ if ($login->isUserLoggedIn () == true) {
 			$sqlInsert = "UPDATE comics
 				SET story_name='$wikiDetails->storyName', plot='$wikiDetails->synopsis', cover_image='images/$wikiDetails->coverFile' wikiUpdated=1
 				WHERE comic_id='$comic_id'";
-			echo $sqlInsert . "<br/><br/>";
+			echo $sqlInsert . "<br/>";
+			echo "<a href=\"../comic.php?comic_id=" . $comic_id . "\">check</a><br/><br/>";
 			set_time_limit(0);
-//			if (mysqli_query ( $connection, $sqlInsert )) {
+//			if (mysqli_query ( $connection, $sql )) {
 //				echo "Record updated successfully with the following information";
 //			} else {
-//				echo "Error: " . $insert_comic_query . "<br>" . mysqli_error ( $connection );
+//				echo "Error: " . $sql . "<br>" . mysqli_error ( $connection );
 //			}
 		}
 	} else {
 		echo "0 results";
 	}
-**/
+
 } else {
 include '../views/not_logged_in.php';
 }
