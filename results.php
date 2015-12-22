a427ccb5ce0737c2448365e5e965ba5ebd9a9af3
Sean Loyless <loyless.sean@gmail.com>
2015-12-21 21:05:57 -0600

    results.php update

--- results.php
+++ results.php
@@ -6,19 +6,8 @@
   $issue_number = filter_input(INPUT_POST, 'issue_number');
   $query = $series_name . " " . $issue_number;
 
-  //$comic = str_replace(' ', '+', $query);
-  //$api_url = "http://marvel.wikia.com/api/v1/Search/List?query=$comic&limit=25&minArticleQuality=10&batch=1&namespaces=0%2C14";
-  //$jsondata = file_get_contents($api_url);
-  //$results = json_decode($jsondata, true);
-
   $returnedResults = new wikiQuery();
-  $returnedResults->wikiSearch($query, $series_name, $issue_number, 25);
-
-
-  //while ($row = $returnedResults->wikiSearchResultTitle) {
-  //	echo "<a href=\"\details.php?wiki_id=" . $returnedResults->wikiSearchResultID . "\">" . $returnedResults->wikiSearchResultTitle . "</a> - ";
-  //	echo "<br>\n";
-  //}
+  $results = $returnedResults->wikiSearch($query, $series_name, $issue_number, 50);
 ?>
 <?php include 'views/header.php';?>
   <div class="container">
@@ -26,7 +15,7 @@
       <div class="col-sm-12">
         <h2>Your Search Results</h2>
         <p>We found the following issues on the Marvel Wikia related to your search:</p>
-        <?php echo $returnedResults->resultsList; ?>
+        <?php echo $results; ?>
       </div>
     </div>
   </div>
