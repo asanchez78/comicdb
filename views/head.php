ba94278a7a9f4cd8524eff8ffc6f8065810736e5
xebix78 <anthony@xebix.com>
2016-01-06 16:16:36 -0600

    mobile icon!

--- views/head.php
+++ views/head.php
@@ -16,3 +16,4 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
   <link rel="stylesheet" href="/styles.css">
+  <link rel="apple-touch-icon-precomposed" href="/assets/pow.png">
