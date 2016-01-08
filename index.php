40ad6fa843288857d2930ee1949f1eb117de2f7a
asanchez78 <anthony@xebix.com>
2016-01-08 15:38:10 -0600

    beginning of multi user functionality!
    added $user variable to seriesList() if ?user=<user> is passed with the URL, the index page will grab that user's books. Otherwise, the regular index page is displayed

--- index.php
+++ index.php
@@ -1,9 +1,38 @@
 <?php
 	require_once('views/head.php');
+	$user = filter_input(INPUT_GET, 'user');
+	if ($user == "") {
 	if ($login->isUserLoggedIn () == true) {
 		$series_list = null;
 		$comics = new comicSearch ();
-		$comics->seriesList ();
+		$comics->seriesList ($user);
+
+		if ($comics->series_list_result->num_rows > 0) {
+			while ( $row = $comics->series_list_result->fetch_assoc () ) {
+				$series_id = $row ['series_id'];
+				$series_name = $row ['series_name'];
+				$series_vol = $row ['series_vol'];
+				$comics->seriesInfo($series_id);
+				$series_issue_count = $comics->series_issue_count;
+				if ($series_issue_count == 1) {
+					$series_issue_count = $series_issue_count . ' Issue';
+				} else {
+					$series_issue_count = $series_issue_count . ' Issues';
+				}
+				$series_cover = $comics->series_latest_cover;
+				if ($series_cover == NULL) {
+					$series_cover = 'assets/nocover.jpg';
+				}
+				$series_list .= '<li class="col-xs-6 col-sm-3 col-md-2"><a href="issues.php?series_id=' . $series_id . '" class="series-info"><img src="/' . $series_cover  . '" /><div class="series-title"><h3>' . $series_name . '</h3></div></a><small>' . $series_issue_count . '</small><div class="volume-number"><span class="count">Vol ' . $series_vol . '</span></div><a href="#" class="button add-button">[Add New]</a><a href="#" class="button edit-button">[Edit]</a></li>';
+			}
+		} else {
+			$series_list = "<li>No Comic Series in database. Perhaps you should <a href=\"/admin/addseries.php\">Add some.</a></li>";
+		}
+	}
+	} else {
+		$series_list = null;
+		$comics = new comicSearch ();
+		$comics->seriesList ($user);
 
 		if ($comics->series_list_result->num_rows > 0) {
 			while ( $row = $comics->series_list_result->fetch_assoc () ) {
@@ -35,9 +64,10 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12">
-				<?php if ($login->isUserLoggedIn () == true) { ?>
+				<?php if ($login->isUserLoggedIn () == true or isset($user)) { ?>
+				
 					<h2>Your Comics</h2>
-					<?php if ($series_list != null) { ?>
+					<?php if (isset($series_list) and $series_list != null) { ?>
 					<ul class="inventory-table row">
 						<?php echo $series_list; ?>
 					</ul>
