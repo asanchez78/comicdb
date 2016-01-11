8120088a5d3f47eb32063773ea73730445143d92
Sean Loyless <loyless.sean@gmail.com>
2016-01-11 11:02:03 -0600

    Forgot to fix WikiaEdit.php!

--- admin/wikiaedit.php
+++ admin/wikiaedit.php
@@ -28,43 +28,43 @@
       </div>
       <div class="col-md-8 col-sm-12">
         <form method="post" action="formprocess.php">
-          <div>
+          <div class="form-group">
             <label for="series_name">Series: </label>
-            <input name="series_name" type="text" maxlength="255" value="<?php echo $dbValues->series_name; ?>" required />
+            <input name="series_name" class="form-control" type="text" maxlength="255" value="<?php echo $dbValues->series_name; ?>" required />
           </div>
-          <div>
+          <div class="form-group">
             <label for="issue_number">Issue Number: </label>
-            <input name="issue_number" type="text" maxlength="255" value="<?php echo $dbValues->issue_number; ?>" required />
+            <input name="issue_number" class="form-control" type="text" maxlength="255" value="<?php echo $dbValues->issue_number; ?>" required />
           </div>
-          <div>
+          <div class="form-group">
             <label for="story_name">Story Name: </label>
-              <input name="story_name" type="text" maxlength="255" value="<?php echo $comic->storyName; ?>" />
+              <input name="story_name" class="form-control" type="text" maxlength="255" value="<?php echo $comic->storyName; ?>" />
           </div>
-          <div>
+          <div class="form-group">
             <label for="released_date">Release Date:</label>
-            <input name="released_date" size="10" maxlength="10" value="<?php echo $dbValues->release_date; ?>" type="text" placeholder="YYYY-MM-DD" />
+            <input class="form-control" name="released_date" size="10" maxlength="10" value="<?php echo $dbValues->release_date; ?>" type="date" placeholder="YYYY-MM-DD" />
           </div>
-          <div class="textarea">
+          <div class="form-group textarea">
             <label for="plot">Plot:</label>
-            <textarea name="plot"><?php echo $comic->synopsis; ?></textarea>
+            <textarea class="form-control" name="plot"><?php echo $comic->synopsis; ?></textarea>
           </div>
-          <div>
+          <div class="form-group">
             <label for="cover_image">Cover Image URL:</label>
-            <input name="cover_image" type="text" maxlength="255" value="<?php echo $comic->coverURL; ?>" />
+            <input class="form-control" name="cover_image" type="text" maxlength="255" value="<?php echo $comic->coverURL; ?>" />
           </div>
-          <div>
+          <div class="form-group">
             <label for="cover_image_file">Destination Filename:</label>
-            <input name="cover_image_file" type="text" maxlength="255" value="<?php echo $comic->coverFile; ?>" />
+            <input class="form-control" name="cover_image_file" type="text" maxlength="255" value="<?php echo $comic->coverFile; ?>" />
           </div>
           <input type="hidden" name="original_purchase" value="<?php echo $dbValues->original_purchase; ?>" />
           <input type="hidden" name="update" value="yes" />
           <input type="hidden" name="comic_id" value="<?php echo $comic_id; ?>" />
           <input type="hidden" name="series_id" value="<?php echo $dbValues->series_id; ?>" />
 
-          <input type="submit" name="submit" value="Submit" />
+          <input type="submit" name="submit" value="Submit" class="btn btn-default form-submit" />
         </form>
       </div>
-      <div class="col-md-4">
+      <div class="col-md-4 issue-image">
         <img src="<?php echo $comic->coverURL; ?>" alt="Cover" />
       </div>
     </div>
