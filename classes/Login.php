e39716a9e91cde21d088cf1f065046742f4aeb9b
Anthony Sanchez <anthony@xebix.com>
2016-01-12 17:03:10 -0600

    created a $_SESSION variable to hold the user_id

--- classes/Login.php
+++ classes/Login.php
@@ -66,7 +66,7 @@ elseif (isset ( $_POST ["login"] )) {
 				
 				// database query, getting all the info of the selected user (allows login via email address in the
 				// username field)
-				$sql = "SELECT user_name, user_email, user_password_hash
+				$sql = "SELECT *
                         FROM users
                         WHERE user_name = '" . $user_name . "' OR user_email = '" . $user_name . "';";
 				$result_of_login_check = $this->db_connection->query ( $sql );
@@ -85,6 +85,7 @@ elseif (isset ( $_POST ["login"] )) {
 						$_SESSION ['user_name'] = $result_row->user_name;
 						$_SESSION ['user_email'] = $result_row->user_email;
 						$_SESSION ['user_login_status'] = 1;
+						$_SESSION ['user_id'] = $result_row->user_id;
 					} else {
 						$this->errors [] = "Wrong password. Try again.";
 					}
