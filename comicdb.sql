cc12bf9b3a820fc69cb66fce0469ec9f0a155128
xebix78 <anthony@xebix.com>
2016-01-18 19:14:49 -0600

    the combination of user_id and coimc_id in the users_comics table must now be unique.

--- comicdb.sql
+++ comicdb.sql
@@ -37,17 +37,17 @@ CREATE TABLE IF NOT EXISTS `comics` (
   `wikiUpdated` int(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`comic_id`),
   UNIQUE KEY `comic_id` (`comic_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=613 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=618 ;
 
 DROP TABLE IF EXISTS `series`;
 CREATE TABLE IF NOT EXISTS `series` (
   `series_id` int(3) NOT NULL AUTO_INCREMENT,
   `series_name` varchar(100) NOT NULL,
   `series_vol` int(3) DEFAULT NULL,
-  PRIMARY KEY (`series_id`),
-  UNIQUE KEY `series_name` (`series_name`),
-  UNIQUE KEY `series_id` (`series_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=41 ;
+  UNIQUE KEY `series_id` (`series_id`),
+  UNIQUE KEY `series_name_2` (`series_name`,`series_vol`),
+  KEY `series_name` (`series_name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=48 ;
 
 DROP TABLE IF EXISTS `storylines`;
 CREATE TABLE IF NOT EXISTS `storylines` (
@@ -83,8 +83,9 @@ CREATE TABLE IF NOT EXISTS `users_comics` (
   `custPlot` varchar(10000) COLLATE utf8_unicode_ci NOT NULL,
   `custCover` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `custStoryName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=569 ;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`,`comic_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=576 ;
 
 DROP TABLE IF EXISTS `writers`;
 CREATE TABLE IF NOT EXISTS `writers` (
