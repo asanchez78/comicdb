919bfbebb0fb7bf7f6a4cfdba9f048da6a0f0abd
Sean Loyless <loyless.sean@gmail.com>
2016-01-25 16:25:17 -0600

    #33 Formatting, fixing a broken <h3>, and removing a call to $comic->publisherList(); that was unneeded.

--- add.php
+++ add.php
@@ -60,53 +60,53 @@
           </form>
         </div>
       <?php } elseif ($issueAdd == true) { ?>
-      <form method="post" action="<?php echo $filename; ?>?type=issue-submit#addissue">
-        <div class="col-sm-12 headline">
-          <h2>Add Issue: <?php echo $series_name; ?> (Vol <?php echo $series_vol; ?>) #<?php echo $issue_number; ?></h2>
-        </div>
-        <div class="col-md-8 col-sm-12">
-          <div class="form-group">
-            <label for="story_name">Story Name: </label>
-            <input class="form-control" name="story_name" type="text" maxlength="255" value="<?php echo $wiki->storyName; ?>" />
+        <form method="post" action="<?php echo $filename; ?>?type=issue-submit#addissue">
+          <div class="col-sm-12 headline">
+            <h2>Add Issue: <?php echo $series_name; ?> (Vol <?php echo $series_vol; ?>) #<?php echo $issue_number; ?></h2>
           </div>
-          <div class="form-group">
-            <label for="released_date">Release Date:</label>
-            <input class="form-control" name="released_date" size="10" maxlength="10" value="" type="date" placeholder="YYYY-MM-DD" />
+          <div class="col-md-8 col-sm-12">
+            <div class="form-group">
+              <label for="story_name">Story Name: </label>
+              <input class="form-control" name="story_name" type="text" maxlength="255" value="<?php echo $wiki->storyName; ?>" />
+            </div>
+            <div class="form-group">
+              <label for="released_date">Release Date:</label>
+              <input class="form-control" name="released_date" size="10" maxlength="10" value="" type="date" placeholder="YYYY-MM-DD" />
+            </div>
+            <div class="form-group form-radio">
+              <label for="original_purchase">Purchased When Released:</label>
+              <fieldset>
+                <input name="original_purchase" id="original-yes" value="1" type="radio" /> <label for="original-yes">Yes</label>
+                <input name="original_purchase" id="original-no" value="0" type="radio" /> <label for="original-no">No</label>
+              </fieldset>
+            </div>
+            <div class="plot form-group">
+              <label for="plot">Plot:</label>
+              <small><a href="#">[edit]</a></small>
+              <?php echo $wiki->synopsis; ?>
+            </div>
           </div>
-          <div class="form-group form-radio">
-            <label for="original_purchase">Purchased When Released:</label>
-            <fieldset>
-              <input name="original_purchase" id="original-yes" value="1" type="radio" /> <label for="original-yes">Yes</label>
-              <input name="original_purchase" id="original-no" value="0" type="radio" /> <label for="original-no">No</label>
-            </fieldset>
+          <div class="col-md-4 issue-image">
+            <img src="<?php echo $wiki->coverURL; ?>" alt="Cover" />
+            <div class="form-group">
+              <label for="cover_image">Cover Image URL</label>
+              <input type="url" class="form-control" name="cover_image" value="<?php echo $wiki->coverURL; ?>" />
+              <small>Enter the URL of the image you wish to use. Default is the cover file from the Wikia entry on this issue.</small>
+              <input type="hidden" name="cover_image_file" value="<?php echo $wiki->coverFile; ?>" />
+            </div>
           </div>
-          <div class="plot form-group">
-            <label for="plot">Plot:</label>
-            <small><a href="#">[edit]</a></small>
-            <?php echo $wiki->synopsis; ?>
+          <input type="hidden" name="series_name" value="<?php echo $series_name; ?>" />
+          <input type="hidden" name="series_vol" value="<?php echo $series_vol; ?>" />
+          <input type="hidden" name="issue_number" value="<?php echo $issue_number; ?>" />
+          <input type="hidden" name="plot" value="<?php echo htmlspecialchars($wiki->synopsis); ?>" />
+          <input type="hidden" name="series_id" value="<?php echo $series_id; ?>" />
+          <input type="hidden" name="wiki_id" value="<?php echo $wiki_id; ?>" />
+          <input type="hidden" name="submitted" value="yes" />
+          <div class="text-center center-block">
+            <button class="btn btn-lg btn-warning form-back"><i class="fa fa-arrow-left"></i> Back</button>
+            <button type="submit" name="submit" class="btn btn-lg btn-danger form-submit"><i class="fa fa-paper-plane"></i> Submit</button>
           </div>
-        </div>
-        <div class="col-md-4 issue-image">
-          <img src="<?php echo $wiki->coverURL; ?>" alt="Cover" />
-          <div class="form-group">
-            <label for="cover_image">Cover Image URL</label>
-            <input type="url" class="form-control" name="cover_image" value="<?php echo $wiki->coverURL; ?>" />
-            <small>Enter the URL of the image you wish to use. Default is the cover file from the Wikia entry on this issue.</small>
-            <input type="hidden" name="cover_image_file" value="<?php echo $wiki->coverFile; ?>" />
-          </div>
-        </div>
-        <input type="hidden" name="series_name" value="<?php echo $series_name; ?>" />
-        <input type="hidden" name="series_vol" value="<?php echo $series_vol; ?>" />
-        <input type="hidden" name="issue_number" value="<?php echo $issue_number; ?>" />
-        <input type="hidden" name="plot" value="<?php echo htmlspecialchars($wiki->synopsis); ?>" />
-        <input type="hidden" name="series_id" value="<?php echo $series_id; ?>" />
-        <input type="hidden" name="wiki_id" value="<?php echo $wiki_id; ?>" />
-        <input type="hidden" name="submitted" value="yes" />
-        <div class="text-center center-block">
-          <button class="btn btn-lg btn-warning form-back"><i class="fa fa-arrow-left"></i> Back</button>
-          <button type="submit" name="submit" class="btn btn-lg btn-danger form-submit"><i class="fa fa-paper-plane"></i> Submit</button>
-        </div>
-      </form>  
+        </form>  
       <?php } elseif ($issueSubmit == true) { ?>
         <div class="add-success col-xs-12 <?php if ($messageNum != 51) { echo 'bg-success'; } else { echo 'bg-danger'; } ?>">
           <div class="success-message">
@@ -115,7 +115,7 @@
                 <img src="<?php echo $cover_image_file; ?>" alt="<?php echo $series_name . '(Vol ' . $series_vol . ') #' . $issue_number; ?> Cover" class="" />
               </div>
               <div class="col-xs-12 col-md-9">
-                <h3><?php echo $series_name; ?> <small>(Vol <?php echo $series_vol; ?>)</small> #<?php echo $issue_number; ?></h2>
+                <h3><?php echo $series_name; ?> <small>(Vol <?php echo $series_vol; ?>)</small> #<?php echo $issue_number; ?></h3>
                 <p><?php if ($messageNum != 51) { echo 'has been added to your collection.'; } else { echo 'already exists in your collection.'; } ?></p>
               </div>
             </div>
@@ -137,7 +137,6 @@
                   $listAllSeries=1;
                   $comic = new comicSearch ();
                   $comic->seriesList ($listAllSeries);
-                  $comic->publisherList ();
                   while ( $row = $comic->series_list_result->fetch_assoc () ) {
                     $list_series_name = $row ['series_name'];
                     $list_series_vol = $row ['series_vol'];
