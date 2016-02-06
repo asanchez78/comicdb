2925610a811c885f5689b2c5c8a1fe58cbda8801
Sean Loyless <loyless.sean@gmail.com>
2016-02-06 02:26:04 -0600

    Some tweaks to icons. Using native styles instead of custom overrides.

--- views/header.php
+++ views/header.php
@@ -21,21 +21,21 @@
     		<div class="collapse navbar-collapse" id="main-nav-collapse">
 					<ul class="nav navbar-nav nolist">
 					<?php if ($login->isUserLoggedIn () == true) { ?>
-						<li class="active"><a href="/"><i class="fa fa-home"></i> Home</a></li>
-						<li><a href="/add.php"><i class="fa fa-plus-circle text-center"></i> Add Items</a></li>
+						<li class="active"><a href="/"><i class="fa fa-fw fa-home"></i> Home</a></li>
+						<li><a href="/add.php"><i class="fa fa-fw fa-plus-circle text-center"></i> Add Items</a></li>
 						
 						<li class="menu-break"><a href="/feed.php"><i class="fa fa-users"></i> User Feed</a></li>
-						<li><a href="/settings.php"><i class="fa fa-cog text-center"></i> Settings</a></li>
+						<li><a href="/settings.php"><i class="fa fa-fw fa-cog text-center"></i> Settings</a></li>
 						
-						<li class="menu-break"><a href="/about.php"><i class="fa fa-question text-center"></i> Who Are We?</a></li>
-						<li><a href="/contact.php"><i class="fa fa-comment text-center"></i> Contact Us</a></li>
-						<li><a href="https://github.com/asanchez78/comicdb/issues" target="_blank"><i class="fa fa-bug text-center"></i> Submit a Bug</a></li>
-						<li><a href="/index.php?logout&m=49"><i class="fa fa-sign-out text-center"></i> Logout</a></li>
+						<li class="menu-break"><a href="/about.php"><i class="fa fa-fw fa-question text-center"></i> Who Are We?</a></li>
+						<li><a href="/contact.php"><i class="fa fa-fw fa-comment text-center"></i> Contact Us</a></li>
+						<li><a href="https://github.com/asanchez78/comicdb/issues" target="_blank"><i class="fa fa-fw fa-bug text-center"></i> Submit a Bug</a></li>
+						<li><a href="/index.php?logout&m=49"><i class="fa fa-fw fa-sign-out text-center"></i> Logout</a></li>
 					<?php } else { ?>
-						<li><a href="/about.php"><i class="fa fa-question text-center"></i> Who Are We?</a></li>
-						<li><a href="/contact.php"><i class="fa fa-comment text-center"></i> Contact Us</a></li>
-						<li class="menu-break"><a href="/admin/register.php?return=<?php echo $current_page; ?>" class="login"><i class="fa fa-plus text-center"></i> Register</a></li>
-						<li><a href="/login.php?return=<?php echo $current_page; ?>" class="login"><i class="fa fa-sign-in text-center"></i> Login</a></li>
+						<li><a href="/about.php"><i class="fa fa-fw fa-question text-center"></i> Who Are We?</a></li>
+						<li><a href="/contact.php"><i class="fa fa-fw fa-comment text-center"></i> Contact Us</a></li>
+						<li class="menu-break"><a href="/admin/register.php?return=<?php echo $current_page; ?>" class="login"><i class="fa fa-fw fa-plus text-center"></i> Register</a></li>
+						<li><a href="/login.php?return=<?php echo $current_page; ?>" class="login"><i class="fa fa-fw fa-sign-in text-center"></i> Login</a></li>
 					<?php } ?>
 					</ul>
 			</nav>
