6bc6cb44a71481310759489ef80292f1b3beeff6
Sean Loyless <loyless.sean@gmail.com>
2016-02-09 11:13:44 -0600

    #77: Fixed Add Comics menu navigation on mobile and adjusted tablet as well.

--- add.php
+++ add.php
@@ -17,40 +17,46 @@
 </head>
 <body>
   <?php include 'views/header.php';?>
-  <div class="add-container">
-    <?php if ($login->isUserLoggedIn () == false) {
-      include (__ROOT__."/views/not_logged_in.php");
-      die ();
-    } ?>
-    <ul class="add-menu nav nav-tabs nav-justified" id="addTabs">
-      <li role="presentation" class="active">
-        <a href="#addSingle" id="form-add-issue" aria-controls="addSingle" role="tab" data-toggle="tab"><i class="fa fa-plus"></i> <span class="hidden-sm hidden-xs">Add Issue</span></a>
-      </li>
-      <li role="presentation">
-        <a href="#addRange" id="form-add-range" aria-controls="addRange" role="tab" data-toggle="tab">
-          <i class="fa fa-hashtag"></i> <span class="hidden-sm hidden-xs">Add Range of Issues</span>
+  <?php if ($login->isUserLoggedIn () == false) {
+    include (__ROOT__."/views/not_logged_in.php");
+    die ();
+  } ?>
+  <div class="row">
+    <ul class="add-menu nav nav-tabs" id="addTabs">
+      <li role="presentation" class="active col-xs-3">
+        <a href="#addSingle" id="form-add-issue" aria-controls="addSingle" role="tab" data-toggle="tab">
+          <i class="fa fa-fw fa-plus"></i> <span class="hidden-sm hidden-xs">Add Issue</span>
         </a>
       </li>
-      <li role="presentation">
-        <a href="#addList" id="form-add-list" aria-controls="addList" role="tab" data-toggle="tab"><i class="fa fa-archive"></i> <span class="hidden-sm hidden-xs">Add List of Issues</span></a>
+      <li role="presentation" class="col-xs-3">
+        <a href="#addRange" id="form-add-range" aria-controls="addRange" role="tab" data-toggle="tab">
+          <i class="fa fa-fw fa-hashtag"></i> <span class="hidden-sm hidden-xs">Add Range of Issues</span>
+        </a>
       </li>
-      <li role="presentation">
-        <a href="#addSeries" id="form-add-series" aria-controls="addSeries" role="tab" data-toggle="tab"><i class="fa fa-folder-open"></i> <span class="hidden-sm hidden-xs">Add Series</span></a>
+      <li role="presentation" class="col-xs-3">
+        <a href="#addList" id="form-add-list" aria-controls="addList" role="tab" data-toggle="tab">
+          <i class="fa fa-fw fa-archive"></i> <span class="hidden-sm hidden-xs">Add List of Issues</span>
+        </a>
+      </li>
+      <li role="presentation" class="col-xs-3">
+        <a href="#addSeries" id="form-add-series" aria-controls="addSeries" role="tab" data-toggle="tab">
+          <i class="fa fa-fw fa-folder-open"></i> <span class="hidden-sm hidden-xs">Add Series</span>
+        </a>
       </li>
     </ul>
-    <div class="tab-content">
-      <?php // ADD SINGLE ISSUE ?>
-      <?php include(__ROOT__.'modules/add_comic/add_issue.php'); ?>
+  </div>
+  <div class="tab-content">
+    <?php // ADD SINGLE ISSUE ?>
+    <?php include(__ROOT__.'/modules/add_comic/add_issue.php'); ?>
 
-      <?php // ADD RANGE ?>
-      <?php include(__ROOT__.'modules/add_comic/add_range.php'); ?>
-      
-      <?php // ADD LIST ?>
-      <?php include(__ROOT__.'modules/add_comic/add_list.php'); ?>
+    <?php // ADD RANGE ?>
+    <?php include(__ROOT__.'/modules/add_comic/add_range.php'); ?>
+    
+    <?php // ADD LIST ?>
+    <?php include(__ROOT__.'/modules/add_comic/add_list.php'); ?>
 
-      <?php // ADD SERIES ?>
-      <?php include(__ROOT__.'modules/add_comic/add_series.php'); ?>
-    </div>
+    <?php // ADD SERIES ?>
+    <?php include(__ROOT__.'/modules/add_comic/add_series.php'); ?>
   </div>
 <?php include 'views/footer.php';?>
 </body>
