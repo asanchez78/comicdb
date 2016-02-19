2eb4c830fe0973caaad882e59867f6ce2520c3f0
Sean Loyless <loyless.sean@gmail.com>
2016-02-19 00:06:03 -0600

    #95: User and count bars functionality done!

--- modules/dashboard/count_bar/count_bar.php
+++ modules/dashboard/count_bar/count_bar.php
@@ -1,20 +1,36 @@
+<?php
+  $userInfo->collectionCount ($userID);
+  $userInfo->seriesCount ($userID);
+  $userInfo->userFollowedBy ($userID);
+  $totalIssues = $userInfo->total_issue_count;
+  $totalSeries = $userInfo->total_series_count;
+
+  if (isset($userInfo->user_follows)) {
+    $followList = preg_split('/\D/', $userInfo->user_follows, NULL, PREG_SPLIT_NO_EMPTY);
+    $followCount = count($followList);
+  }
+
+  if (isset($userInfo->followerCount)) {
+    $followerCount = $userInfo->followerCount;
+  }
+?>
 <div data-module="count_bar">
   <div class="row">
     <div class="col-xs-6 col-lg-3">
       <strong><i class="fa fa-fw fa-hashtag"></i> Total Comics</strong>
-      <span class="big-red">1114</span>
+      <span class="big-red"><?php echo $totalIssues; ?></span>
     </div>
     <div class="col-xs-6 col-lg-3">
       <strong><i class="fa fa-fw fa-archive"></i> Total Series</strong>
-      <span class="big-red">206</span>
+      <span class="big-red"><?php echo $totalSeries; ?></span>
     </div>
     <div class="col-xs-6 col-lg-3">
       <strong><i class="fa fa-fw fa-users"></i> Following</strong>
-      <span class="big-red">238</span>
+      <span class="big-red"><?php if (isset($userInfo->user_follows) && $userInfo->user_follows != '') { echo $followCount; } ?></span>
     </div>
     <div class="col-xs-6 col-lg-3">
       <strong><i class="fa fa-fw fa-users"></i> Followers</strong>
-      <span class="big-red">894</span>
+      <span class="big-red"><?php if (isset($userInfo->followerCount)) { echo $followerCount; } ?></span>
     </div>
     <div class="col-xs-12">
       <a href="/profile.php"><i class="fa fa-fw fa-eye"></i> View your collection</a>
--- modules/user_bar/user_bar.php
+++ modules/user_bar/user_bar.php
@@ -1,3 +1,20 @@
+<?php
+  $userInfo = new comicSearch ();
+  $userInfo->userMeta($userID);
+
+  if (isset($userInfo->user_first_name)) {
+    $first_name = $userInfo->user_first_name;  
+  } else {
+    $first_name = $userName;
+  }
+
+  if (isset($userInfo->user_avatar)) {
+    $avatar = $userInfo->user_avatar;
+  } else {
+    $gravatar_hash = $userEmail;
+    $avatar = '//www.gravatar.com/avatar/' . $gravatar_hash . '?s=200&d=' . urlencode('http://comicmanager.com/assets/avatar-deadpool.png');
+  }
+?>
 <div data-module="user_bar">
   <div class="row">
     <div class="col-xs-6">
@@ -18,8 +35,8 @@
     <div class="col-xs-6 col-md-3 col-lg-3 user-profile-button text-right">
       <a href="/profile.php">
         <span class="profile-icon"><i class="fa fa-user"></i></span>
-        <span class="profile-name">Sean</span>
-        <span class="profile-avatar"><img src="/assets/avatar-sean.jpg" alt="Sean" class="img-responsive img-circle" /></span>
+        <span class="profile-name"><?php echo $first_name; ?></span>
+        <span class="profile-avatar"><img src="<?php echo $avatar;?>" alt="Sean" class="img-responsive img-circle" /></span>
       </a>
     </div>
   </div>
