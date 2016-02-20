cc507ec2f4a31ea5c0fa2ecdb1bf139c5cc017b7
Sean Loyless <loyless.sean@gmail.com>
2016-02-20 14:03:51 -0600

    #95: Pull List module completed. Moved some styling around.

--- views/dashboard.php
+++ views/dashboard.php
@@ -3,10 +3,10 @@
     <?php include 'modules/dashboard/count_bar/count_bar.php'; ?>
     <?php include 'modules/dashboard/carousel_bar/carousel_bar.php'; ?>
     <div class="row">
-      <div class="col-md-6">
+      <div class="col-md-6 dash-half">
         <?php include 'modules/dashboard/quick_add/quick_add.php'; ?>
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6 dash-half">
         <?php include 'modules/dashboard/pull_list/pull_list.php'; ?>
       </div>
     </div>
