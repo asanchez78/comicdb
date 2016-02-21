1244149a1ee82fc82a6a58c5989432e9ebe41140
Sean Loyless <loyless.sean@gmail.com>
2016-02-20 18:57:56 -0600

    Fix for follow button and adding meta stuff to the head for better search engine results

--- views/head.php
+++ views/head.php
@@ -30,5 +30,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="/styles.css">
-  <link rel="apple-touch-icon-precomposed" href="/assets/pow.png">
+  <link rel="apple-touch-icon-precomposed" href="/assets/logo.png">
   <script src="/scripts/jquery-2.2.0.min.js"></script>
+  <meta name="description" content="POW! Comic Book Manager is your best place to organize and display your comic book collection!">
+
+  <!-- Facebook Opengraph -->
+  <meta property="og:site_name" content="POW! Comic Book Manager" />
+  <meta property="og:url" content="http://comicmanager.com"/>
+  <meta property="og:title" content="POW! Comic Book Manager" />
+  <meta property="og:description" content="POW! Comic Book Manager is your best place to organize and display your comic book collection!" />
+  <meta property="og:image" content="http://comicmanager.com/assets/logo.png" />
+  <meta property="og:type" content="website" />
