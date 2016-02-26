1597bcb0963369b726924b2137adcdec51927bd0
Sean Loyless <loyless.sean@gmail.com>
2016-02-26 11:43:05 -0600

    #95: User Feed sidebar and page styled to include first names and avatars.

--- feed.php
+++ feed.php
@@ -7,11 +7,8 @@
   <?php include 'views/header.php';?>
   <div class="row">
     <div class="col-xs-12">
-      <header class="headline"><h2>Updates</h2></header>
-      <?php
-      $feedLength = 30;
-      include 'modules/feed/feed.php';
-      ?>
+      <header class="headline"><h2>Recent Activity from your Followers</h2></header>
+      <?php include 'modules/feed/feed.php'; ?>
     </div>
   </div>
   <?php include 'views/footer.php';?>
